7182b458c071ceb375cd5a47f50a252d6d8d5aae
Juan Galarce <j6alarce@gmail.com>
2021-02-01 11:51:01 -0300

    Se crear el componente formulario y se trabaja con Modulos CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario/Formulario';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       />
 
       <div className="container withe">
-        <h1>Formulario</h1>
+        <Formulario/>
       </div>
     </Fragment>
   );
