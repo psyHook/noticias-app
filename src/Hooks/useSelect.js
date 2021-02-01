278a1a4fc7586dccf656a1ebebc0baaf056e33f0
Juan Galarce <j6alarce@gmail.com>
2021-02-01 12:13:03 -0300

    Mostrando las categorias en el select

--- src/Hooks/useSelect.js
+++ src/Hooks/useSelect.js
@@ -2,11 +2,19 @@ import React, { useState } from "react";
 
 const useSelect = (stateInicial, opciones) => {
   // State del custom hook
-  const [state, setState] = useState("");
+  const [state, setState] = useState(stateInicial);
 
   const SelectNoticias = () => (
-    <select className="browser-default">
-      <option value="">- Seleccione -</option>
+    <select
+      className="browser-default"
+      value={state}
+      onChange={(e) => setState(e.target.value)}
+    >
+      {opciones.map((opcion) => (
+        <option key={opcion.value} value={opcion.value}>
+          {opcion.label}
+        </option>
+      ))}
     </select>
   );
 
--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,23 +1,33 @@
-import React from "react";
-import styles from "../Formulario/Formulario.module.css";
-import useSelect from "../../Hooks/useSelect";
+import React from 'react';
+import styles from '../Formulario/Formulario.module.css';
+import useSelect from '../../Hooks/useSelect';
 
 const Formulario = () => {
-  const [categoria, SelectNoticias] = useSelect();
+  const OPCIONES = [
+    { value: 'general', label: 'General ' },
+    { value: 'business', label: 'Negocios ' },
+    { value: 'entertainment', label: 'Entretenimiento ' },
+    { value: 'health', label: 'Salud ' },
+    { value: 'science', label: 'Ciencia ' },
+    { value: 'sports', label: 'Deportes ' },
+    { value: 'technology', label: 'Tecnologia ' },
+  ];
+
+  const [categoria, SelectNoticias] = useSelect('general', OPCIONES);
 
   return (
     <div className={`${styles.buscador} row`}>
-      <div className="col s12 m8 offset-m2">
+      <div className='col s12 m8 offset-m2'>
         <form>
           <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
 
           <SelectNoticias />
 
-          <div className="input-field col s12">
+          <div className='input-field col s12'>
             <input
-              type="submit"
+              type='submit'
               className={`btn-large amber darken-2 ${styles.btn_block}`}
-              value="Buscar"
+              value='Buscar'
             />
           </div>
         </form>
