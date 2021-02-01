e838cfb56704a126f50bd42ec620956c18bfa27c
Juan Galarce <j6alarce@gmail.com>
2021-02-01 12:39:33 -0300

    Consultando la api, leyendo los datos y guardandolos en el state noticias

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario/Formulario';
 
 function App() {
-
   // Definir categoria y noticias
   const [categoria, setCategoria] = useState('');
+  const [noticias, setNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=51da28fd21934604bf2c09f41eb373cc`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      setNoticias(noticias.articles);
+    };
+    consultarAPI();
+  }, [categoria]);
 
   return (
     <Fragment>
-      <Header
-        titulo="Buscador de Noticias"
-      />
+      <Header titulo='Buscador de Noticias' />
 
-      <div className="container withe">
-        <Formulario setCategoria={setCategoria}/>
+      <div className='container withe'>
+        <Formulario setCategoria={setCategoria} />
       </div>
     </Fragment>
   );
